2e659fbb038c43c1c43b579c70780d4c3d4998fa
TigerQuantOffical <64720142+tigerquantoffical@users.noreply.github.com>
2020-05-03 18:57:04 +0800

    Add Setup Configuration

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='TigerQuant',
+    version='0.1',
+    packages=[''],
+    url='www.tigerquant.com',
+    license='GPL',
+    author='TigerQuant',
+    author_email='TigerQuant@gmail.com',
+    description='China Stock Data Library'
+)
